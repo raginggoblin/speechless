e3876d8a7d5a1a231852ea157a8ee9627dda776c
Raging Goblin <raginggoblin@xs4all.nl>
2015-07-22 19:05:25 +0200

    SoundEffect as enum

--- src/main/java/raging/goblin/speechless/speech/SoundEffect.java
+++ src/main/java/raging/goblin/speechless/speech/SoundEffect.java
@@ -19,7 +19,6 @@
 
 package raging.goblin.speechless.speech;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -28,47 +27,56 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.prefs.Preferences;
+import java.util.stream.Collectors;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import raging.goblin.speechless.Messages;
-import raging.goblin.speechless.SpeechLessProperties;
 
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-public class SoundEffect {
+public enum SoundEffect {
 
-   public enum Effect {
-      TRACT_SCALER, F0_SCALE, F0_ADD, RATE, ROBOT, WHISPER, STADIUM, CHORUS, FIR_FILTER, JET_PILOT
-   };
+   TRACT_SCALER(Messages.getInstance().get("tract_scaler"), Messages.getInstance().get("tract_scaler_help"),
+         "TractScaler amount:%.2f;", createDefaultLevelsMap(Arrays.asList("amount"), Arrays.asList(1.5))),
+
+   F0_SCALE(Messages.getInstance().get("f0_scaling"), Messages.getInstance().get("f0_scaling_help"),
+         "F0Scale f0Scale:%.2f;", createDefaultLevelsMap(Arrays.asList("f0Scale"), Arrays.asList(2.0))),
+
+   F0_ADD(Messages.getInstance().get("f0_add"), Messages.getInstance().get("f0_add_help"), "F0Add f0Add:%.2f;",
+         createDefaultLevelsMap(Arrays.asList("f0Add"), Arrays.asList(50.0))),
+
+   RATE(Messages.getInstance().get("rate"), Messages.getInstance().get("rate_help"), "Rate durScale:%.2f;",
+         createDefaultLevelsMap(Arrays.asList("durScale"), Arrays.asList(1.5))),
+
+   ROBOT(Messages.getInstance().get("robot"), Messages.getInstance().get("robot_help"), "Robot amount:%.2f;",
+         createDefaultLevelsMap(Arrays.asList("amount"), Arrays.asList(100.0))),
+
+   WHISPER(Messages.getInstance().get("whisper"), Messages.getInstance().get("whisper_help"), "Whisper amount:%.2f;",
+         createDefaultLevelsMap(Arrays.asList("amount"), Arrays.asList(100.0))),
+
+   STADIUM(Messages.getInstance().get("stadium"), Messages.getInstance().get("stadium_help"), "Stadium amount:%.2f;",
+         createDefaultLevelsMap(Arrays.asList("amount"), Arrays.asList(100.0))),
+
+   CHORUS(Messages.getInstance().get("chorus"), Messages.getInstance().get("chorus_help"),
+         "Chorus delay1:%.2f;amp1:%.2f;delay2:%.2f;amp2:%.2f;delay3:%.2f;amp3:%.2f;", createDefaultLevelsMap(
+               Arrays.asList("delay1", "amp1", "delay2", "amp2", "delay3", "amp3"),
+               Arrays.asList(466.0, 0.54, 600.0, -0.10, 250.0, 0.30))),
+
+   FIR_FILTER(Messages.getInstance().get("fir_filter"), Messages.getInstance().get("fir_filter_help"),
+         "FIRFilter type:%.2f;fc1:%.2f;fc2:%.2f;", createDefaultLevelsMap(Arrays.asList("type", "fc1", "fc2"),
+               Arrays.asList(3.0, 500.0, 2000.0))),
+
+   JET_PILOT(Messages.getInstance().get("jet_pilot"), Messages.getInstance().get("jet_pilot_help"), "JetPilot;",
+         new HashMap<>());
 
    private static final Preferences PREFERENCES = Preferences.userNodeForPackage(SoundEffect.class);
-   private static final Messages MESSAGES = Messages.getInstance();
-   private static final List<SoundEffect> EFFECTS = new ArrayList<>();
 
-   @Getter
-   private Effect effect;
+   private String visualName;
    @Getter
    private String helpText;
-   @Getter
    private String format;
    private Map<String, Double> defaultLevels;
-
-   public static List<SoundEffect> getSoundEffects(SpeechLessProperties properties) {
-      if (EFFECTS.isEmpty()) {
-         EFFECTS.add(createTractScaler());
-         EFFECTS.add(createF0Add());
-         EFFECTS.add(createF0Scale());
-         EFFECTS.add(createRate());
-         EFFECTS.add(createRobot());
-         EFFECTS.add(createWhisper());
-         EFFECTS.add(createStadium());
-         EFFECTS.add(createChorus());
-         EFFECTS.add(createFirFilter());
-         EFFECTS.add(createJetPilot());
-      }
-      return EFFECTS;
-   }
 
    public Set<String> getLevelKeys() {
       return new HashSet<>(defaultLevels.keySet());
@@ -76,12 +84,12 @@ public class SoundEffect {
 
    public void setLevel(String key, double value) {
       if (defaultLevels.containsKey(key)) {
-         PREFERENCES.putDouble(effect + "_" + key, value);
+         PREFERENCES.putDouble(name() + "_" + key, value);
       }
    }
 
    public double getLevel(String key) {
-      return PREFERENCES.getDouble(effect + "_" + key, defaultLevels.get(key));
+      return PREFERENCES.getDouble(name() + "_" + key, defaultLevels.get(key));
    }
 
    public void setDefaultLevels() {
@@ -91,63 +99,21 @@ public class SoundEffect {
    }
 
    public boolean isEnabled() {
-      return PREFERENCES.getBoolean(effect.name() + "_enabled", false);
+      return PREFERENCES.getBoolean(name() + "_enabled", false);
    }
 
    public void setEnabled(boolean enabled) {
-      PREFERENCES.putBoolean(effect.name() + "_enabled", enabled);
+      PREFERENCES.putBoolean(name() + "_enabled", enabled);
    }
 
-   private static SoundEffect createTractScaler() {
-      return new SoundEffect(Effect.TRACT_SCALER, MESSAGES.get("tract_scaler_help"), "TractScaler amount:%.2f;",
-            createDefaultLevelsMap(Arrays.asList("amount"), Arrays.asList(1.5)));
+   @Override
+   public String toString() {
+      return visualName;
    }
 
-   private static SoundEffect createJetPilot() {
-      return new SoundEffect(Effect.JET_PILOT, MESSAGES.get("jet_pilot_help"), "JetPilot;", new HashMap<>());
-   }
-
-   private static SoundEffect createF0Scale() {
-      return new SoundEffect(Effect.F0_SCALE, MESSAGES.get("f0_scaling_help"), "F0Scale f0Scale:%.2f;",
-            createDefaultLevelsMap(Arrays.asList("f0Scale"), Arrays.asList(2.0)));
-   }
-
-   private static SoundEffect createF0Add() {
-      return new SoundEffect(Effect.F0_ADD, MESSAGES.get("f0_add_help"), "F0Add f0Add:%.2f;", createDefaultLevelsMap(
-            Arrays.asList("f0Add"), Arrays.asList(50.0)));
-   }
-
-   private static SoundEffect createRate() {
-      return new SoundEffect(Effect.RATE, MESSAGES.get("rate_help"), "Rate durScale:%.2f;", createDefaultLevelsMap(
-            Arrays.asList("durScale"), Arrays.asList(1.5)));
-   }
-
-   private static SoundEffect createRobot() {
-      return new SoundEffect(Effect.ROBOT, MESSAGES.get("robot_help"), "Robot amount:%.2f;", createDefaultLevelsMap(
-            Arrays.asList("amount"), Arrays.asList(100.0)));
-   }
-
-   private static SoundEffect createWhisper() {
-      return new SoundEffect(Effect.WHISPER, MESSAGES.get("whisper_help"), "Whisper amount:%.2f;",
-            createDefaultLevelsMap(Arrays.asList("amount"), Arrays.asList(100.0)));
-   }
-
-   private static SoundEffect createStadium() {
-      return new SoundEffect(Effect.STADIUM, MESSAGES.get("stadium_help"), "Stadium amount:%.2f;",
-            createDefaultLevelsMap(Arrays.asList("amount"), Arrays.asList(100.0)));
-   }
-
-   private static SoundEffect createChorus() {
-      return new SoundEffect(Effect.CHORUS, MESSAGES.get("chorus_help"),
-            "Chorus delay1:%.2f;amp1:%.2f;delay2:%.2f;amp2:%.2f;delay3:%.2f;amp3:%.2f;", createDefaultLevelsMap(
-                  Arrays.asList("delay1", "amp1", "delay2", "amp2", "delay3", "amp3"),
-                  Arrays.asList(466.0, 0.54, 600.0, -0.10, 250.0, 0.30)));
-   }
-
-   private static SoundEffect createFirFilter() {
-      return new SoundEffect(Effect.FIR_FILTER, MESSAGES.get("fir_filter_help"),
-            "FIRFilter type:%.2f;fc1:%.2f;fc2:%.2f;", createDefaultLevelsMap(Arrays.asList("type", "fc1", "fc2"),
-                  Arrays.asList(3.0, 500.0, 2000.0)));
+   public String toMaryTTSString() {
+      List<Double> values = defaultLevels.keySet().stream().map(k -> getLevel(k)).collect(Collectors.toList());
+      return String.format(format, values.toArray());
    }
 
    private static Map<String, Double> createDefaultLevelsMap(List<String> keys, List<Double> values) {
