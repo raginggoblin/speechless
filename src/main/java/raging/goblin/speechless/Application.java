7598ca8321050eb182d72c42c9bafdbcbeebbf13
raginggoblin <raginggoblin@xs4all.nl>
2015-12-11 22:01:21 +0100

    Update Application.java

--- src/main/java/raging/goblin/speechless/Application.java
+++ src/main/java/raging/goblin/speechless/Application.java
@@ -15,6 +15,8 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with SpeechLess.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ aap
  */
 
 package raging.goblin.speechless;
